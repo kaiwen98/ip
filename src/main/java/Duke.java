5739c89b8766ecc2296c38569585532966c5a111
kaiwen98 <e0406836@u.nus.edu>
2020-08-20 16:35:03 +0800

    Increment level 0: Added message class and functions to print hello and bye messages

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,37 @@
 public class Duke {
+    public static Message message;
+    public static int charCount = 30;
+
+    /**
+     * Prints partition line following each message
+     */
+    public static void drawPartition(){
+        String partitionLine = new String(new char[charCount]).replace("\0", "_");
+        System.out.println(partitionLine);
+    }
+
+    /**
+     * Prints the corresponding messages onto console depending on input parameter.
+     *
+     * @param command a string of a simple root word to represent a particular message
+     */
+    public static void printMessage(String command){
+        String output = "";
+        switch(command){
+            case "hello":
+                output = message.messageHello;
+                break;
+            case "bye":
+                output = message.messageBye;
+                break;
+            default:
+                output = message.messageErr;
+                break;
+        }
+        System.out.println(output);
+        drawPartition();
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +39,9 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        drawPartition();
+        printMessage("hello");
+        printMessage("bye");
     }
 }
+
---
+++ src/main/java/Message.java
@@ -0,0 +1,9 @@
+/**
+ * Class recording all possible messages printable on console
+ */
+public class Message {
+    public static String messageHello = "Hello! I'm Duke\n"
+            + "What can I do for you?";
+    public static String messageBye = "Bye. Hope to see you again soon!";
+    public static String messageErr = "Command not recognized.";
+}
